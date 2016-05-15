4b106b3380b8f74dca387b1f3a21b141bf92ede8
Evan <efreethy@gmail.com>
2016-05-15 10:08:46 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/favorites', function(req, res){
+  var data = fs.readFileSync('./data.json');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+// app.get('/favorites', function(req, res){
+//   console.log(req.body.name, req.body.oid);
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return;
+//   }
+//
+//   var data = JSON.parse(fs.readFileSync('./data.json'));
+//   data.push(req.body);
+//   fs.writeFile('./data.json', JSON.stringify(data));
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
